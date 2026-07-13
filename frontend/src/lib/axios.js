e569456f55f0e6b9a6d46d705d963ba9fb1ce57c
benjiwis <bcifuentes@ing.ucsc.cl>
2026-07-13 08:31:18 -0400

    aPI

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const axiosClient = axios.create({
-    baseURL: 'https://api.rentacarencoyhaique2.cl/api',
+    baseURL: 'http://127.0.0.1:8000/api',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
